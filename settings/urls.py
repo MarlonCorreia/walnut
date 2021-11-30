4f4309689db92685647e48dd6a8f22fc5f882669
MarlonSimas <malilocsimas26@gmail.com>
2021-11-30 20:30:43 -0300

    feat: Add health check

--- settings/urls.py
+++ settings/urls.py
@@ -2,14 +2,23 @@
 settings URL Configuration
 """
 
+###
+# Libraries
+###
+
 from django.contrib import admin
 
 from django.conf.urls import url, include
+
+from helpers.health_check import health_check
 
 admin.autodiscover()
 
 urlpatterns = [
     url(f'^admin/', admin.site.urls),
     url(r'^', include('accounts.urls')),
-    url(r'^', include('walnut.urls'))
+    url(r'^', include('walnut.urls')),
+
+    #Health Check
+    url(r'health-check/$', health_check, name='health_check'),
 ]
